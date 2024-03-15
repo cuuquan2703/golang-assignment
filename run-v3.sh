e02ee08721b0c885f2d0d17b016f251dfc305757
CQUAN <cuuquan2703@gmail.com>
2024-03-15 20:21:19 +0700

    sh file

--- run-v3.sh
+++ run-v3.sh
@@ -2,6 +2,7 @@
 URL = $1
 git checkout v2
 echo "Migrate up to v3 . . ."
+migrate -path db/migration -database $1 goto 2
 migrate -path db/migration -database $1 -verbose up 1
 echo "Running server . . ."
 go run main.go
